9c72e7b4a62cd96bdb385fff943433fb39bb286b
lemonilemon <87176609+lemonilemon@users.noreply.github.com>
2021-07-16 23:48:45 +0800

    Update router.js

--- 檔案總管/D/讀書心得/63567272/神秘的檔案/router.js
+++ 檔案總管/D/讀書心得/63567272/神秘的檔案/router.js
@@ -4,43 +4,43 @@ function route(group) {
 			return "https://gather.town/invite?token=mzcydF1N";
 			break;
 		case 2:
-			return "https://gather.town/invite?token=Joypvhw7";
+			return "https://gather.town/invite?token=xEMbQMuh";
 			break;
 		case 3:
-			return "https://gather.town/invite?token=4iUmN3Rr";
+			return "https://gather.town/invite?token=7bl3lgKL";
 			break;
 		case 4:
-			return "https://gather.town/invite?token=rmtAWaaQ";
+			return "https://gather.town/invite?token=RcDUaGPg";
 			break;
 		case 5:
-			return "https://gather.town/invite?token=MPbbX7si";
+			return "https://gather.town/invite?token=DayY4bOJ";
 			break;
 		case 6:
-			return "https://gather.town/invite?token=PLDXea2G";
+			return "https://gather.town/invite?token=a3MLAppI";
 			break;
 		case 7:
-			return "https://gather.town/invite?token=sSfZV8yj";
+			return "https://gather.town/invite?token=DMIkRXc8";
 			break;
 		case 8:
-			return "https://gather.town/invite?token=EdY1ZOp5";
+			return "https://gather.town/invite?token=n3mJbB3f";
 			break;
 		case 9:
-			return "https://gather.town/invite?token=GSwxDznX";
+			return "https://gather.town/invite?token=UNGs8dxV";
 			break;
 		case 10:
-			return "https://gather.town/invite?token=eFkjvNpF";
+			return "https://gather.town/invite?token=xGv3GN7R";
 			break;
 		case 11:
-			return "https://gather.town/invite?token=CsQdfByV";
+			return "https://gather.town/invite?token=RNNXVvFC";
 			break;
 		case 12:
-			return "https://gather.town/invite?token=LU0aPUsS";
+			return "https://gather.town/invite?token=oQD4EsI6";
 			break;
 		case 13:
-			return "https://gather.town/invite?token=yDj4LhVi";
+			return "https://gather.town/invite?token=zmMghNoe";
 			break;
 		case 14:
-			return "https://gather.town/invite?token=u4BcV4hz";
+			return "https://gather.town/invite?token=j2gklQnn";
 			break;
 		default:
 			return "請輸入有效數據";
